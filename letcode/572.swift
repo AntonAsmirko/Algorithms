9974cb80bd1dae57f737174583659c086c615a0b
AntonAsmirko <LoLkingXXX@gmail.com>
2021-07-27 01:51:36 +1000

    572

---
+++ letcode/572.swift
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func isSubtree(_ root: TreeNode?, _ subRoot: TreeNode?) -> Bool {
+        return dfs(root, subRoot)
+    }
+    
+    func dfs(_ node: TreeNode?, _ subRoot: TreeNode?) -> Bool {
+        if isSame(node, subRoot){
+            return true
+        }
+        return node != nil && (dfs(node!.right, subRoot) || dfs(node!.left, subRoot))
+    }
+    
+    func isSame(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
+        if(p == nil && q == nil){
+            return true
+        }
+        if(p == nil && q != nil || p != nil && q == nil || p!.val != q!.val){
+            return false
+        }
+        return isSame(p!.left, q!.left) && isSame(p!.right, q!.right)
+    }
+}
